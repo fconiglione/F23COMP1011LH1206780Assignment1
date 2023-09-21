35f10d0c18a2df734b71949c1502e2f4fc4a1ee1
fconiglione <122475578+fconiglione@users.noreply.github.com>
2023-09-21 13:47:51 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.f23comp1011lh1206780assignment1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.f23comp1011lh1206780assignment1 to javafx.fxml;
+    exports com.example.f23comp1011lh1206780assignment1;
+}
