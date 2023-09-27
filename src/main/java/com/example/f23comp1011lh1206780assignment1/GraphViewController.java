c2ac7c6fc3ae1b440e7eb7f8259fb60c3e3e3b90
fconiglione <122475578+fconiglione@users.noreply.github.com>
2023-09-26 22:10:07 -0400

    TableViewController and GraphViewController complete, except for switching views ability

--- src/main/java/com/example/f23comp1011lh1206780assignment1/GraphViewController.java
+++ src/main/java/com/example/f23comp1011lh1206780assignment1/GraphViewController.java
@@ -1,7 +1,38 @@
 package com.example.f23comp1011lh1206780assignment1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 
-public class GraphViewController {
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class GraphViewController implements Initializable {
+
+    @FXML
+    private BarChart<String, Number> barChart;
+
+    @FXML
+    void switchToGraphView() {
+
+    }
+
+    @FXML
+    void switchToTableView() {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ArrayList<Language> languages = DBUtility.getLanguages();
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        for (Language language : languages) {
+            series.getData().add(new XYChart.Data<>(language.getLanguage(), language.getPopularity()));
+        }
+        barChart.getData().add(series);
+    }
 }
--- src/main/java/com/example/f23comp1011lh1206780assignment1/TableViewController.java
+++ src/main/java/com/example/f23comp1011lh1206780assignment1/TableViewController.java
@@ -1,4 +1,56 @@
 package com.example.f23comp1011lh1206780assignment1;
 
-public class TableViewController {
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class TableViewController implements Initializable {
+
+    @FXML
+    private TableView<Language> tableView;
+
+    @FXML
+    private TableColumn<Language, String> languageColumn;
+
+    @FXML
+    private TableColumn<Language, Double> popularityColumn;
+
+    @FXML
+    void switchToGraphView() {
+
+    }
+
+    @FXML
+    void switchToTableView() {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ArrayList<Language> languages = DBUtility.getLanguages();
+
+        ObservableList<Language> observableLanguages = FXCollections.observableArrayList(languages);
+
+        tableView.setItems(observableLanguages);
+
+        languageColumn.setCellValueFactory(cellData -> {
+            String language = cellData.getValue().getLanguage();
+            return new SimpleStringProperty(language);
+        });
+
+        popularityColumn.setCellValueFactory(cellData -> {
+            double popularity = cellData.getValue().getPopularity();
+            return new SimpleDoubleProperty(popularity).asObject();
+        });
+    }
+
 }
